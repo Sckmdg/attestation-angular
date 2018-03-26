e5ca5bc8b98a875dc222f4c2d33e40c31a7a66c8
aminov <rustam.aminov@sibers.com>
2018-03-26 18:48:26 +0600

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/common/footer/footer.component';
 import { SidebarComponent } from './components/common/sidebar/sidebar.component';
