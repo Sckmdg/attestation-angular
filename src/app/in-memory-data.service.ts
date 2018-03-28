7e74372dd75e1b1034b2df44c6d430bd1af9a19e
aminov <rustam.aminov@sibers.com>
2018-03-28 12:06:07 +0600

    add loader

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,13 +1,21 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+// import test from './slider-templates/test.html'
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
+    // console.log(test);
     const slides = [
       {
         id: 0,
         order: 1,
         title: 'Test',
-        template: `<p>test</p>`,
+        template: 'test'
+      },
+      {
+        id: 1,
+        order: 2,
+        title: 'Test2',
+        template: 'test2'
       }
     ];
     return {slides};
