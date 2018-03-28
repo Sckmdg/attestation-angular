57a534340b6b766be4664fb3f5c1175fc2d0256a
aminov <rustam.aminov@sibers.com>
2018-03-28 16:36:06 +0600

    fix styles

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,14 +8,68 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 0,
         order: 1,
-        title: 'Test',
-        template: 'test'
+        title: 'Test1',
+        template: 'test1'
       },
       {
         id: 1,
         order: 2,
         title: 'Test2',
         template: 'test2'
+      },
+      {
+        id: 2,
+        order: 2,
+        title: 'Test3',
+        template: 'test3'
+      },
+      {
+        id: 3,
+        order: 2,
+        title: 'Test4',
+        template: 'test4'
+      },
+      {
+        id: 4,
+        order: 2,
+        title: 'Test5',
+        template: 'test5'
+      },
+      {
+        id: 5,
+        order: 2,
+        title: 'Test6',
+        template: 'test6'
+      },
+      {
+        id: 6,
+        order: 2,
+        title: 'Test7',
+        template: 'test7'
+      },
+      {
+        id: 8,
+        order: 2,
+        title: 'Test9',
+        template: 'test9'
+      },
+      {
+        id: 9,
+        order: 2,
+        title: 'Test10',
+        template: 'test10'
+      },
+      {
+        id: 10,
+        order: 2,
+        title: 'Test11',
+        template: 'test11'
+      },
+      {
+        id: 11,
+        order: 2,
+        title: 'Test12',
+        template: 'test12'
       }
     ];
     return {slides};
