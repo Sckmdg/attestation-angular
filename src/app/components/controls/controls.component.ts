79313bcc1241d6994a9a66e15ac011be938ee34e
aminov <rustam.aminov@sibers.com>
2018-04-03 12:50:47 +0600

    start creating edit form

--- src/app/components/controls/controls.component.ts
+++ src/app/components/controls/controls.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component, EventEmitter, Input, Output } from '@angular/core';
+import { Slide } from "../root-slide/slide";
 
 @Component({
   selector: 'app-controls',
@@ -6,10 +7,20 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./controls.component.css']
 })
 export class ControlsComponent {
+  @Input() slide: Slide;
   @Output() addSlide: EventEmitter<any> = new EventEmitter();
+  isEdit: boolean;
 
   addHandler(): void {
     this.addSlide.emit(null);
+  }
+
+  toggleEditWindow(): void {
+    this.isEdit = !this.isEdit;
+  }
+
+  closeEditWindow(): void {
+    this.isEdit = false;
   }
 
   constructor() { }
